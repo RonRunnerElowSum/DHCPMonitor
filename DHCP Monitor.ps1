9e3eb23a4db966be523e2ee633481a311635efa2
RonRunnerElowSum <97925118+RonRunnerElowSum@users.noreply.github.com>
2022-03-23 14:24:16 -0400

    Update DHCP Monitor.ps1

--- DHCP Monitor.ps1
+++ DHCP Monitor.ps1
@@ -176,44 +176,44 @@ function PunchIt {
                 [string]$BlacklistedMACFoundLogs = $TodaysDHCPLogs | Where-Object {$BlacklistedMACAddresses -contains $_."MAC Address"} | Format-Table -AutoSize | Out-String
                 try{
                     $IPToRevoke = ($TodaysDHCPLogs | Where-Object {$BlacklistedMACAddresses -contains $_."MAC Address"})."IP Address" | Select-Object -First 1
-                    Remove-DhcpServerv4Lease -ComputerName $Env:ComputerName -IPAddress $IPToRevoke
+                    Remove-DhcpServerv4Lease -ComputerName $([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName) -IPAddress $IPToRevoke
                 }
                 catch{
-                    Write-Warning "The DHCP server ($Env:ComputerName) leased an IP to a blacklised device and the DHCP Monitor failed to revoke the lease...address immediately!`r`n`r`n$BlacklistedMACFoundLogs`r`n`r`nRevoke lease error:`r`n$($global:intErr++)Error #:$global:intErr`r`n$Error$($Error.Clear())"
-                    Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Warning" -LogMessage "The DHCP server ($Env:ComputerName) leased an IP to a blacklised device and the DHCP Monitor failed to revoke the lease...address immediately!`r`n`r`n$BlacklistedMACFoundLogs`r`n`r`nRevoke lease error:`r`n$($global:intErr++)Error #:$global:intErr`r`n$Error$($Error.Clear())"
+                    Write-Warning "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) leased an IP to a blacklised device and the DHCP Monitor failed to revoke the lease...address immediately!`r`n`r`n$BlacklistedMACFoundLogs`r`n`r`nRevoke lease error:`r`n$($global:intErr++)Error #:$global:intErr`r`n$Error$($Error.Clear())"
+                    Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Warning" -LogMessage "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) leased an IP to a blacklised device and the DHCP Monitor failed to revoke the lease...address immediately!`r`n`r`n$BlacklistedMACFoundLogs`r`n`r`nRevoke lease error:`r`n$($global:intErr++)Error #:$global:intErr`r`n$Error$($Error.Clear())"
                     $Recipients | ForEach-Object {
-                        Send-Alert -ToAddress $_ -FromAddress $SenderAddress -Subject "($Env:ComputerName) leased an IP to a blacklised device!" -Body "The DHCP server ($Env:ComputerName) leased an IP to a blacklised device and the DHCP Monitor failed to revoke the lease...address immediately!`r`n`r`n$BlacklistedMACFoundLogs`r`n`r`nRevoke lease error:`r`n$($global:intErr++)Error #:$global:intErr`r`n$Error$($Error.Clear())"
+                        Send-Alert -ToAddress $_ -FromAddress $SenderAddress -Subject "($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) leased an IP to a blacklised device!" -Body "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) leased an IP to a blacklised device and the DHCP Monitor failed to revoke the lease...address immediately!`r`n`r`n$BlacklistedMACFoundLogs`r`n`r`nRevoke lease error:`r`n$($global:intErr++)Error #:$global:intErr`r`n$Error$($Error.Clear())"
                     }
                 }
-                Write-Host "The DHCP server ($Env:ComputerName) leased an IP to a blacklised device and the DHCP Monitor successfiully revoked the lease!`r`n`r`n$BlacklistedMACFoundLogs"
-                Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Information" -LogMessage "The DHCP server ($Env:ComputerName) leased an IP to a blacklised device and the DHCP Monitor successfiully revoked the lease!`r`n`r`n$BlacklistedMACFoundLogs"
+                Write-Host "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) leased an IP to a blacklised device and the DHCP Monitor successfiully revoked the lease!`r`n`r`n$BlacklistedMACFoundLogs"
+                Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Information" -LogMessage "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) leased an IP to a blacklised device and the DHCP Monitor successfiully revoked the lease!`r`n`r`n$BlacklistedMACFoundLogs"
                 $Recipients | ForEach-Object {
-                    Send-Alert -ToAddress $_ -FromAddress $SenderAddress -Subject "($Env:ComputerName) leased an IP to a blacklised device and successfully revoked it!" -Body "The DHCP server ($Env:ComputerName) leased an IP to a blacklised device and the DHCP Monitor successfiully revoked the lease!`r`n`r`n$BlacklistedMACFoundLogs"
+                    Send-Alert -ToAddress $_ -FromAddress $SenderAddress -Subject "($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) leased an IP to a blacklised device and successfully revoked it!" -Body "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) leased an IP to a blacklised device and the DHCP Monitor successfiully revoked the lease!`r`n`r`n$BlacklistedMACFoundLogs"
                 }
             }   
         }
         if($TodaysDHCPLogs | Where-Object {$_.ID -eq 14}){
             [string]$EmptyDHCPPoolLogs = $TodaysDHCPLogs | Where-Object {$_.ID -eq 14} | Format-Table -AutoSize | Out-String
-            Write-Host "The DHCP server ($Env:ComputerName) has run out of DHCP leases...address immediately!`r`n`r`n$EmptyDHCPPoolLogs"
-            Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Warning" -LogMessage "The DHCP server ($Env:ComputerName) has run out of DHCP leases...address immediately!`r`n`r`n$EmptyDHCPPoolLogs"
+            Write-Host "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has run out of DHCP leases...address immediately!`r`n`r`n$EmptyDHCPPoolLogs"
+            Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Warning" -LogMessage "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has run out of DHCP leases...address immediately!`r`n`r`n$EmptyDHCPPoolLogs"
             $Recipients | ForEach-Object {
-                Send-Alert -ToAddress $_ -FromAddress $SenderAddress -Subject "($Env:ComputerName) has run out of DHCP leases!" -Body "The DHCP server ($Env:ComputerName) has run out of DHCP leases...address immediately!`r`n`r`n$EmptyDHCPPoolLogs"
+                Send-Alert -ToAddress $_ -FromAddress $SenderAddress -Subject "($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has run out of DHCP leases!" -Body "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has run out of DHCP leases...address immediately!`r`n`r`n$EmptyDHCPPoolLogs"
             }
         }
         if($TodaysDHCPLogs | Where-Object {$_.ID -eq 61}){
             [string]$RogueADDHCPServerLogs = $TodaysDHCPLogs | Where-Object {$_.ID -eq 61} | Format-Table -AutoSize | Out-String
-            Write-Host "The DHCP server ($Env:ComputerName) has found another DHCP server on the network that belongs to the AD domain...address immediately!`r`n`r`n$RogueADDHCPServerLogs"
-            Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Warning" -LogMessage "The DHCP server ($Env:ComputerName) has found another DHCP server on the network that belongs to the AD domain...address immediately!`r`n`r`n$RogueADDHCPServerLogs"
+            Write-Host "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has found another DHCP server on the network that belongs to the AD domain...address immediately!`r`n`r`n$RogueADDHCPServerLogs"
+            Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Warning" -LogMessage "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has found another DHCP server on the network that belongs to the AD domain...address immediately!`r`n`r`n$RogueADDHCPServerLogs"
             $Recipients | ForEach-Object {
-                Send-Alert -ToAddress $_ -FromAddress $SenderAddress -Subject "($Env:ComputerName) has found another DHCP server!" -Body "The DHCP server ($Env:ComputerName) has found another DHCP server on the network that belongs to the AD domain...address immediately!`r`n`r`n$RogueADDHCPServerLogs"
+                Send-Alert -ToAddress $_ -FromAddress $SenderAddress -Subject "($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has found another DHCP server!" -Body "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has found another DHCP server on the network that belongs to the AD domain...address immediately!`r`n`r`n$RogueADDHCPServerLogs"
             }
         }
         if($TodaysDHCPLogs | Where-Object {$_.ID -eq 62}){
             [string]$RogueDHCPServerLogs = $TodaysDHCPLogs | Where-Object {$_.ID -eq 62} | Format-Table -AutoSize | Out-String
-            Write-Host "The DHCP server ($Env:ComputerName) has found another DHCP server on the network...address immediately!`r`n`r`n$RogueDHCPServerLogs"
-            Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Warning" -LogMessage "The DHCP server ($Env:ComputerName) has found another DHCP server on the network...address immediately!`r`n`r`n$RogueDHCPServerLogs"
+            Write-Host "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has found another DHCP server on the network...address immediately!`r`n`r`n$RogueDHCPServerLogs"
+            Write-MSPLog -LogSource "MSP DHCP Monitor" -LogType "Warning" -LogMessage "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has found another DHCP server on the network...address immediately!`r`n`r`n$RogueDHCPServerLogs"
             $Recipients | ForEach-Object {
-                Send-Alert -ToAddress $Recipients -FromAddress $SenderAddress -Subject "($Env:ComputerName) has found another DHCP server!" -Body "The DHCP server ($Env:ComputerName) has found another DHCP server on the network...address immediately!`r`n`r`n$RogueDHCPServerLogs"
+                Send-Alert -ToAddress $Recipients -FromAddress $SenderAddress -Subject "($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has found another DHCP server!" -Body "The DHCP server ($([System.Net.Dns]::GetHostByName($Env:ComputerName).HostName)) has found another DHCP server on the network...address immediately!`r`n`r`n$RogueDHCPServerLogs"
             }
         }
 
